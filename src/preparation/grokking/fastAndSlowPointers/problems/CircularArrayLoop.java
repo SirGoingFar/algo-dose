f6c4ac990bbd4a6b3521083860e86c34ea7fa72c
Akintunde <akintunde_olanrewaju@yahoo.co.uk>
2022-08-20 15:35:20 +0100

    add appropriate link

--- src/preparation/grokking/fastAndSlowPointers/problems/CircularArrayLoop.java
+++ src/preparation/grokking/fastAndSlowPointers/problems/CircularArrayLoop.java
@@ -5,12 +5,14 @@ package preparation.grokking.fastAndSlowPointers.problems;
  * number ‘M’ at a particular index. Now, if ‘M’ is positive we will move forward ‘M’ indices and if ‘M’ is negative
  * move backwards ‘M’ indices. You should assume that the array is circular which means two things:
  * <ol>
- *     <li>If, while moving forward, we reach the end of the array, we will jump to the first element to continue the movement.<li/>
- *     <li>If, while moving backward, we reach the beginning of the array, we will jump to the last element to continue the movement.<li/>
- * <ol/>
+ *     <li>If, while moving forward, we reach the end of the array, we will jump to the first element to continue the movement.</li>
+ *     <li>If, while moving backward, we reach the beginning of the array, we will jump to the last element to continue the movement.</li>
+ * </ol>
  * Write a method to determine if the array has a cycle. The cycle should have more than one element and should follow one direction which means the cycle should not contain both forward and backward movements.
  * <p>
- * See more <a href=https://www.educative.io/courses/grokking-the-coding-interview/3j5GD3EQMGM>here</a>
+ * See more <a href=https://www.educative.io/courses/grokking-the-coding-interview/3jY0GKpPDxr>here</a>
+ * <p>
+ * See solution <a href=https://www.educative.io/courses/grokking-the-coding-interview/qAo438WozV7>here</a>
  */
 public class CircularArrayLoop {
 
